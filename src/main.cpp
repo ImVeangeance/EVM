9949c7c96dc0af39478fc8aed4bbb35fc892507c
PlsDeleteMyLife <cdranton@gmail.com>
2019-03-19 12:45:11 +0700

    Last upd

--- src/main.cpp
+++ src/main.cpp
@@ -3,39 +3,21 @@
 
 int main(void)
 {
-	char act;
 	int value, rows = 50, cols = 60;
-	mt_setfgcolor(PINK);
-	mt_setbgcolor(GREEN);
 	sc_memoryInit();
 	sc_regInit();
-	//sc_memorySet(-1, 228);
-	//sc_regGet(ErrorMemoryOut, &value);
-	//std::cout << "Value - " << value << "\n";
-	//sc_memorySave("pojiloibinarnik");
-	//sc_memoryLoad("pojiloibinarnik");
-	//std::cout.unsetf(std::ios::dec);
-	//std::cout.setf(std::ios::hex);	
-	//sc_memoryPrint();
-	//std::cout.unsetf(std::ios::hex);
-	//std::cout.setf(std::ios::dec);
-	sc_regSet(ErrorMemoryOut, 0); // UNSET OF ERROR
-	sc_memorySet(0, 1488);
-	sc_regGet(ErrorMemoryOut, &value);
-	std::cout << "Value - " << value << "\n";
+	mt_clrscr();
+	mt_setfgcolor(PINK);
+	mt_setbgcolor(GREEN);
 	sc_memorySave("pojiloibinarnik");
 	sc_memoryLoad("pojiloibinarnik");
-	std::cout.unsetf(std::ios::dec);
-	std::cout.setf(std::ios::hex);
-	std::cout << std::endl << "CLS!";
-	std::cin >> act;
-	mt_clrscr();
-	sc_memoryPrint();
-	sc_regPrint(5, 100);
-	std::cout.unsetf(std::ios::hex);
-	std::cout.setf(std::ios::dec);
+	sc_memoryPrint(3, 3);
+	mt_setbgcolor(PINK);
+	mt_setfgcolor(BLUE);
+	sc_regPrint(3, 80);
 	//mt_gotoXY(100, 100);
 	//mt_getscreensize(&rows, &cols);
 	std::cout << std::endl;
+	mt_gotoXY(999, 1);
 	return 0;
 }
