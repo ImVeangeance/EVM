a7adc97351dc2a4dd3aaf0f9f2fd3143d34c8145
PlsDeleteMyLife <cdranton@gmail.com>
2019-03-22 16:26:14 +0700

    Started new header-file and functions

---
+++ src/bc_header.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include 'sc_header.hpp'
+#include 'mt_header.hpp'
+
+int bc_printA (char *str)
+{
+	//
+}
+
+int bc_printbigchar(int [2], int x, int y, enum colors, enum colors)
+{
+	//
+}
